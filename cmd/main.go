f40e8ece9e8bf3c1caca2ac4aca3a6db17de5ad4
Dmitriy Tereshin <dm.teryoshin@gmail.com>
2023-05-17 09:50:34 +0300

    draft

--- cmd/main.go
+++ cmd/main.go
@@ -18,6 +18,8 @@ func main() {
 	}
 
 	flag.StringVar(&cfg.ServerAddress, "a", cfg.ServerAddress, "server address")
+	flag.StringVar(&cfg.SecretKey, "k", cfg.SecretKey, "secret key")
+	flag.StringVar(&cfg.LicenseKeyHash, "h", cfg.LicenseKeyHash, "license key hash")
 	flag.Parse()
 
 	log.Fatal(server.Serve(&cfg))
